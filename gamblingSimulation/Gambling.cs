cb3aa94d58b1977b9c18cfc3b44e17e32c397fdb
“teji1998” <tejasvinirpk@gmail.com>
2020-12-03 10:11:15 +0530

    Added:Declared the constants for stake and bet

---
+++ gamblingSimulation/Gambling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gamblingSimulation
+{
+    class Gambling
+    {
+        public const int STAKE = 100;
+        public const int BET = 1;
+    }
+}
