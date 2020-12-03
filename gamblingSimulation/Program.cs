17700cb064eeec406a224448fd1d29de5c2fd46e
“teji1998” <tejasvinirpk@gmail.com>
2020-12-03 10:03:31 +0530

    Added:Welcome message

---
+++ gamblingSimulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gamblingSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
